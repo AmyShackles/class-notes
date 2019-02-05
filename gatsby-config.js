4e71498901fe5d7bee2ff64090e0bfd432ed8a01
Amy Shackles <amyshackles@gmail.com>
2019-02-04 17:23:25 -0800

    Undo

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,7 +60,35 @@ module.exports = {
         icon: config.favicon,
       },
     },
-    'gatsby-plugin-offline',
+    {
+      resolve: 'gatsby-plugin-offline',
+      options: {
+        cacheId: 'gatsby-plugin-offline',
+        // Don't cache-bust JS or CSS files, and anything in the static directory,
+        // since these files have unique URLs and their contents will never change
+        dontCacheBustUrlsMatching: /(\.js$|\.css$|static\/)/,
+        runtimeCaching: [
+          {
+            // Use cacheFirst since these don't need to be revalidated (same RegExp
+            // and same reason as above)
+            urlPattern: /(\.js$|\.css$|static\/)/,
+            handler: 'cacheFirst',
+          },
+          {
+            // Add runtime caching of various other page resources
+            urlPattern: /^https?:.*\.(png|jpg|jpeg|webp|svg|gif|tiff|js|woff|woff2|json|css)$/,
+            handler: 'staleWhileRevalidate',
+          },
+          {
+            // Google Fonts CSS (doesn't end in .css so we need to specify it)
+            urlPattern: /^https?:\/\/fonts\.googleapis\.com\/css/,
+            handler: 'staleWhileRevalidate',
+          },
+        ],
+        skipWaiting: false,
+        clientsClaim: true,
+      },
+    },
     'gatsby-plugin-netlify',
   ],
 };
