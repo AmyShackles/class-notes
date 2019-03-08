4214c366db5858a39922af36d5a8c1d10c5e1adf
Amy Shackles <amyshackles@gmail.com>
2019-03-08 15:40:07 -0800

    Trying to pull css out of BlogList file

---
+++ src/styles/BlogList.js
@@ -0,0 +1,84 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.article`
+  margin: auto;
+`;
+
+export const Image = styled.div`
+  margin: auto;
+  position: relative;
+  box-shadow: ${props => props.theme.shadow.feature.small.default};
+  transition: ${props => props.theme.transitions.boom.transition};
+  border-radius: ${props => props.theme.borderRadius.default};
+  min-height: 300px;
+  img {
+    border-radius: ${props => props.theme.borderRadius.default};
+  }
+  &:hover {
+    box-shadow: ${props => props.theme.shadow.feature.small.hover};
+    transform: scale(1.04);
+  }
+  a {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    border-radius: ${props => props.theme.borderRadius.default};
+    > div {
+      position: static !important;
+    }
+    > div > div {
+      position: static !important;
+    }
+    &:focus {
+      outline: none;
+      box-shadow: 0 0 0 5px ${props => props.theme.colors.primary.dark};
+    }
+  }
+  flex-basis: 100%;
+  max-width: 100%;
+  width: 100%;
+  @media (max-width: 800px) {
+    flex-basis: 100%;
+    max-width: 100%;
+    width: 100%;
+    margin-bottom: 1.5rem;
+  }
+  @media (max-width: 500px) {
+    min-height: 200px;
+  }
+`;
+
+export const Information = styled.div`
+  h1 {
+    display: inline-block;
+    color: ${props => props.theme.colors.black.base};
+    transition: all ${props => props.theme.transitions.default.duration};
+    &:hover {
+      color: ${props => props.theme.colors.primary.base};
+    }
+  }
+  text-align: center;
+  flex-basis: 100%;
+  max-width: 100%;
+  word-wrap: break-word;
+  word-break: break-word;
+  width: 100%;
+  @media (max-width: 800px) {
+    flex-basis: 100%;
+    max-width: 100%;
+    width: 100%;
+  }
+`;
+
+export const Date = styled.div`
+  margin-top: 1rem;
+  font-size: 12px;
+  color: ${props => props.theme.colors.black.lighter};
+`;
+
+export const Title = styled.h1`
+  margin: 0;
+  font-size: 24px;
+`;
