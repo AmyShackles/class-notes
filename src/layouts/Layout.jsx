6c9133a34cc0043368601e7068aee011ad69a841
Amy Shackles <amyshackles@gmail.com>
2019-03-08 18:14:09 -0800

    Tried to separate styles for Layout

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,64 +1,11 @@
 import React, { Fragment } from 'react';
 import { ThemeProvider } from 'emotion-theming';
-import { injectGlobal } from 'react-emotion';
 import PropTypes from 'prop-types';
 import 'typeface-open-sans';
 import 'typeface-candal';
 import { SEO } from 'components';
 import { NavBar, Footer } from 'layouts';
 import theme from '../../config/theme';
-import headroom from '../styles/headroom';
-
-injectGlobal`
-
-  *, *:before, *:after {
-    box-sizing: inherit;
-  }
-  html {
-    text-rendering: optimizeLegibility;
-    overflow-x: hidden;
-    box-sizing: border-box;
-    -ms-overflow-style: scrollbar;
-    -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-  }
-  html, body {
-    width: 100%;
-    height: 100%;
-    margin: 0;
-    padding: 0;
-  }
-
-  body {
-    display: flex;
-    flex-direction: column;
-    min-height: 100vh;
-  }
-  a {
-    color: ${theme.colors.link};
-    transition: color .5s;
-    text-decoration: none;
-  }
-  a:hover {
-    text-decoration: none;
-    color: ${theme.colors.linkHover};
-  }
-  h1{
-    font-family: ${theme.fontFamily.heading};
-  }
-  table {
-    table-layout: fixed;
-    overflow: scroll;
-    width: initial !important;
-  }
-  td, th {
-    padding: 10px !important;
-    width: max-content;
-  }
-
-  ${headroom}
-`;
 
 const Layout = ({ children }) => (
   <ThemeProvider theme={theme}>
