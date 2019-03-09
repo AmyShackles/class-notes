1fabdae4424d090401a083260f5a10951c152b02
Amy Shackles <amyshackles@gmail.com>
2019-03-08 18:24:36 -0800

    Moved styles from index

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,23 +2,9 @@ import React from 'react';
 import { graphql } from 'gatsby';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
-import styled from 'react-emotion';
+import { PostWrapper } from '../styles/index.js';
 import { Header, PostList } from 'components';
 import { Layout } from 'layouts';
-
-const PostWrapper = styled.div`
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  justify-content: space-between;
-  margin: 4rem 4rem 1rem 4rem;
-  @media (max-width: 1000px) {
-    margin: 4rem 2rem 1rem 2rem;
-  }
-  @media (max-width: 700px) {
-    margin: 4rem 1rem 1rem 1rem;
-  }
-`;
 
 const Index = ({ data }) => {
   const { edges } = data.allMarkdownRemark;
---
+++ src/styles/index.js
@@ -0,0 +1,15 @@
+import styled from 'react-emotion';
+
+export const PostWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  margin: 4rem 4rem 1rem 4rem;
+  @media (max-width: 1000px) {
+    margin: 4rem 2rem 1rem 2rem;
+  }
+  @media (max-width: 700px) {
+    margin: 4rem 1rem 1rem 1rem;
+  }
+`;
