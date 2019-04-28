e1a0a1a6eb890b3e0388c8cc5de1f080d5560469
Amy Shackles <amyshackles@gmail.com>
2019-04-28 11:01:14 -0700

    Fix config

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,7 +30,7 @@ module.exports = {
           {
             resolve: 'gatsby-remark-prismjs',
             options: {
-              classPrefix: 'language-',
+              classPrefix: 'language-js',
               inlineCodeMarker: null,
               aliases: {},
               showLineNumbers: true,
